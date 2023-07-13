85b97d18e5b27eb4f8b5daeee8389d246469e056
Guilherme Souza <ghostgui60@gmail.com>
2023-07-13 14:44:19 -0300

    First Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gui_g/mvc-crud',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '967d94495fc85eb05f9b63efe0cdd1066bbe364a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gui_g/mvc-crud' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '967d94495fc85eb05f9b63efe0cdd1066bbe364a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
